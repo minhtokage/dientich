5a531936912d1c4288f66899bd367ff1a8122908
unknown <dinhminhamazing@gmail.com>
2022-02-23 10:33:49 +0700

    dientich

---
+++ index.js
@@ -0,0 +1,8 @@
+let chieuCao
+let chieurong
+chieuCao = prompt('Nhập chiều cao');
+chieurong =prompt('Nhập chiều rộng');
+let Cao = parseInt(chieuCao);
+let rong = parseInt(chieurong);
+let dienTich = Cao*rong
+document.write('Diện tích là: '+dienTich)
